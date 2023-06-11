4bec6d6a732cdb1b9352116c37f1190d4e3fbb2b
Anne-lise Emig <anneliseemig@gmail.com>
2023-06-11 18:10:05 -0500

    improved solution for two sum sorted

--- algorithms/two-sum-ii.ts
+++ algorithms/two-sum-ii.ts
@@ -28,7 +28,10 @@ Output: [1,2]
 Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
 */
 
-function twoSum(numbers: number[], target: number): number[] | undefined {
+// first attempt
+// time complexity: O(n^2)
+// space complexity: O(1)
+function twoSum(numbers: number[], target: number): number[] {
     // initialize first pointer
     let i : number = 0;
     // while pointer is less than length of the array and element is less than target
@@ -49,8 +52,31 @@ function twoSum(numbers: number[], target: number): number[] | undefined {
         // increment pointer
         i++;
     }
+    return [0, 0]
 };
 
 // console.log(twoSum([2,3,4], 6))
 // console.log(twoSum([2,7,11,15], 9))
 console.log(twoSum([-1, -1], -2))
+
+// second attempt properly using two pointers
+function twoSumII (numbers: number[], target: number): number[]{
+    // initialize two pointers
+    let left: number = 0;
+    let right: number = numbers.length - 1;
+    // while second pointer is greater than first
+    while (left < right){
+        // if sum equals target, return array of indexes [i, j] for 1-indexed array
+        if (numbers[left] + numbers[right] === target) return [left + 1, right + 1];
+        // if sum is greater than target, decrement right pointer
+        if (numbers[left] + numbers[right] > target) right--;
+        // if sum is less than target, increment left pointer
+        if (numbers[left] + numbers[right] < target) left++;
+    }
+    // return [0, 0] if no sum matching target is found
+    return [0, 0]
+}
+
+console.log(twoSumII([2,3,4], 6))
+console.log(twoSumII([2,7,11,15], 9))
+console.log(twoSumII([-1, -1], -2))
