701838b6605a368ad218d6282559501389931741
Anne-lise Emig <anneliseemig@gmail.com>
2023-07-07 11:35:56 -0500

    additional twosum solution in python

--- algorithms/two-sum.py
+++ algorithms/two-sum.py
@@ -1,4 +1,5 @@
 class Solution:
+    # first solution (basic)
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         # iterate through list of numbers
         for i in range(len(nums) - 1):
@@ -7,3 +8,19 @@ class Solution:
                 # if two numbers equal sum, return [i, j]
                 if nums[i] + nums[j] == target: 
                     return [i, j]
+    def twoSumII(self, nums: list[int], target: int) -> list[int]:
+         # store seen values in a dictionary
+         seen = {}
+         # iterate through numbers in list
+         for i, el in enumerate(nums):
+             # calculate remainder
+             remainder = target - el
+             # check if remainder exists in seen dictionary
+             if remainder in seen:
+                 return [seen[remainder], i]
+             else: seen[el] = i
+
+print(Solution.twoSum(Solution, [3,2,4], 6)) # [1, 2]
+print(Solution.twoSum(Solution, [3,2,3], 6)) # [0, 2]
+print(Solution.twoSumII(Solution, [3,2,4], 6)) # [1, 2]
+print(Solution.twoSumII(Solution, [3,2,3], 6)) # [0, 2]
